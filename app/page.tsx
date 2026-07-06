6540a6827fc1e4219ab4bad6abe38f33a298ae65
Aleksandar Stanisic <ciandre23@gmail.com>
2026-07-06 14:57:26 +0200

    Add Team page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
-
 export default function Home() {
+  console.log("Hello from the Home page!");
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1>Welcome to Next.js!</h1>
