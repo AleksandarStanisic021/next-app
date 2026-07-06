d325eb251d6a280a560d502cb52ade50b47d21d3
Aleksandar Stanisic <ciandre23@gmail.com>
2026-07-06 15:12:28 +0200

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,11 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+      <body className="min-h-full flex flex-col">
+        <p className="text-lg bg-teal-800 text-amber-300">hi</p>
+        {children}
+      </body>
     </html>
   );
 }
