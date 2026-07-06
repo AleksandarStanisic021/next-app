e3a7910c0e3867dd5f247d4831f623b27be924fb
Aleksandar Stanisic <ciandre23@gmail.com>
2026-07-06 18:45:27 +0200

    fix

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,6 +1,6 @@
 export default function Team() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center  ">
+    <div className="flex flex-col flex-1 items-center justify-center dark:bg-black ">
       <h3 className="text-3xl font-bold">Our Team</h3>
     </div>
   );
