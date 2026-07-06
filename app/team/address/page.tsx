d9840d87336fcb6bc4c0d5f9c5eaaa85dfa4b526
Aleksandar Stanisic <ciandre23@gmail.com>
2026-07-06 15:08:04 +0200

    address component

---
+++ app/team/address/page.tsx
@@ -0,0 +1,7 @@
+export default function Address() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-orange-500 font-sans dark:bg-black">
+      <h3 className="text-3xl font-bold">Our Address</h3>
+    </div>
+  );
+}
