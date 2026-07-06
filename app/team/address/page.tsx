8825aa9c0ebc054459a2038ba87d6230d1051eb7
Aleksandar Stanisic <ciandre23@gmail.com>
2026-07-06 16:36:09 +0200

    update Address component with address details and modify Layout structure

--- app/team/address/page.tsx
+++ app/team/address/page.tsx
@@ -2,6 +2,8 @@ export default function Address() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-orange-500 font-sans dark:bg-black">
       <h3 className="text-3xl font-bold">Our Address</h3>
+      <p className="text-lg">123 Main Street</p>
+      <p className="text-lg">Anytown, USA 12345</p>
     </div>
   );
 }
--- app/team/layout.tsx
+++ app/team/layout.tsx
@@ -1,6 +1,7 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-green-500 font-sans dark:bg-red">
+    <div>
+      <h1 className="text-3xl font-bold text-amber-100">Hi Layout</h1>
       {children}
     </div>
   );
