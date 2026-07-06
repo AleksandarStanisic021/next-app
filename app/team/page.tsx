f25e6b497642af0e82204ce99bea5b4843302664
Aleksandar Stanisic <ciandre23@gmail.com>
2026-07-06 15:00:41 +0200

    fix

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Team() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-orange-500 font-sans dark:bg-black">
-      <h1 className="text-3xl font-bold">Our Team</h1>
+      <h3 className="text-3xl font-bold">Our Team</h3>
     </div>
   );
 }
